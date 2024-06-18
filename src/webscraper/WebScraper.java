60dcf80b9c6ff4e47a4efcfed6410dd625c95a3d
silva3265 <silva3265@gmail.com>
2024-06-18 13:43:23 -0300

    Web Scraping: buscando correspondências com Pattern e Matcher

---
+++ src/webscraper/WebScraper.java
@@ -0,0 +1,24 @@
+package webscraper;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class WebScraper {
+
+	public static void main(String[] args) {
+		String html = "<a href=\"mailto:joao@gmail.com\">\n   joao@gmail.com  </a>\n"
+				+ "<a>\n   abc@algaworks.com</a><a>xyz@algaworks.com</a>" + "<strong>maria@algaworks.com</strong>";
+//        System.out.println(html);
+
+//        String regex = "<strong>(.*)</strong>";
+		String regex = "<.*?>\\s*(?<email>[\\w.-]+@[a-z\\d.-]+\\.[a-z]{2,3})\\s*</.*?>"; //'?' - teimoso
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(html);
+
+		while (matcher.find()) { // find() - localiza o proximo (enquanto estiver encontrando)
+//            System.out.println(matcher.group(1));
+			System.out.println(matcher.group("email"));
+		}
+	}
+
+}
